d050903c4efae11855479077aeb48a74e6772108
unlogisch04 <98281608+unlogisch04@users.noreply.github.com>
2024-06-01 22:50:37 +0200

    Add Github Token Authentication (#1)
    * Add Github Token Authentication
    
    * Usea startWith instead of lastIndexOf
    
    * removed a doublepoint (colon)
    
    * Lets defaults be defaults

--- backend/src/config/config.service.ts
+++ backend/src/config/config.service.ts
@@ -74,7 +74,16 @@ export class ConfigService {
   }
 
   public getGitHubAuth() {
-    return encode(this.getValue("GITHUB_AUTH", true));
+    var sauth = this.getValue("GITHUB_AUTH", true);
+    if ( sauth.startsWith("github_pat") ||
+         sauth.startsWith("ghp_"))
+    {
+      return `Bearer ${sauth}`;
+    }
+    else
+    {
+      return `Basic ${encode(sauth)}`;
+    }
   }
 
   public getHostUrl() {
